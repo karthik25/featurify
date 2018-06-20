2f2615e5d79ad3962bb267aa4f247960a2558fc1
karthik25 <karthika25@gmail.com>
2018-06-19 21:29:42 -0400

    splitting contracts to its own project

--- Pack_NuGet.bat
+++ Pack_NuGet.bat
@@ -1,2 +1,4 @@
-rmdir /Q /S NuGet
+rmdir /Q /S src\Featurify.Contracts\NuGet
+rmdir /Q /S src\Featurify\NuGet
+dotnet pack src\Featurify.Contracts --configuration release --output NuGet
 dotnet pack src\Featurify --configuration release --output NuGet
